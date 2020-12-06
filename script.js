b11f43e9bf07d486c5f953cac96cc929dd233998
Juan Romo <juanromowork@gmail.com>
2020-12-06 02:00:03 -0600

    Add function to generate a 6 deck

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const generateSixDeck = function () {
+  const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+  const values = [
+    'Ace',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'Jack',
+    'Queen',
+    'King',
+  ];
+
+  const sixDeck = [];
+
+  for (let deck = 1; deck <= 6; deck++) {
+    for (let suit = 0; suit < suits.length; suit++) {
+      for (let value = 0; value < values.length; value++) {
+        sixDeck.push({ suit: suits[suit], value: values[value] });
+      }
+    }
+  }
+
+  return sixDeck;
+};
+
+const sixDeck = generateSixDeck();
+
+console.log(sixDeck);
+
+// TODO: shuffle cards
